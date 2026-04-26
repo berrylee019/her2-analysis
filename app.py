9cc6e7fd9b3e23f96c7c646a6296a7f8e0c302f2
Lee ByungSeo <bslee@yahoo.com>
2026-04-26 22:34:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def get_her2_mutations():
             {"op": "in", "content": {"field": "genes.symbol", "value": ["ERBB2"]}}
         ]
     }
-    params = {"filters": json.dumps(filters), "fields": "consequence.transcript.aa_change,occurrence.case.submitter_id", "format": "JSON", "size": "500"}
+    params = {"filters": json.dumps(filters), "fields": "consequence.transcript.aa_change,occurrence.case.submitter_id", "format": "JSON", "size": "1000"}
     try:
         r = requests.get(ssm_url, params=params)
         hits = r.json()['data']['hits']
